b6376b689e1ed3453ae8658fe2c6a96084279ea9
cuzknothz <cuzknothz@gmail.com>
2021-10-31 17:22:32 +0700

    fix height mobile

--- src/components/Stories/StoriesWrapper.tsx
+++ src/components/Stories/StoriesWrapper.tsx
@@ -3,7 +3,7 @@ import { Story } from '@/components/Stories/Story';
 
 export const StoriesWrapper = () => {
   return (
-    <div className="w-screen sm:w-[615px] border-[1px] h-[100px] border-ins-border flex items-center space-x-3 pl-3 bg-black/20">
+    <div className="w-screen sm:w-[615px] border-[1px] h-[100px] border-ins-border flex items-center space-x-3 pl-3 ">
       <Story />
     </div>
   );
