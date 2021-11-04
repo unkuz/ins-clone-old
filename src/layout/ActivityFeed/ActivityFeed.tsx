a0d10414f987a6844e61b2a9a2957ca41b4547ac
cuzknothz <cuzknothz@gmail.com>
2021-11-04 11:27:29 +0700

    fix

--- src/layout/ActivityFeed/ActivityFeed.tsx
+++ src/layout/ActivityFeed/ActivityFeed.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const ActivityFeed: React.FC = () => {
   return (
-    <div className="absolute bottom-14 right-0 md:top-12 md:right-52 z-30  ">
+    <div className="absolute bottom-32 right-0 md:top-12 md:right-52 z-30  ">
       <div className="w-[300px] md:w-[400px] h-[300px] mx-auto border-[1px] border-ins-border bg-white flex justify-center items-center">
         🚧 This feature is currently unavailable ! 🚧
       </div>
